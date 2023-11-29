a5086091620ae41e31784f7bc4dbdbdc102ed0a4
Ahmed Atef <AA2201416@tkh.edu.eg>
2023-11-29 16:14:45 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -9,3 +9,11 @@ from io import StringIO
 
 # Import the functions and dictionary from the Morse code translator script
 from morsecodeapp import text_to_morse, morse_to_text, morse_code_dict, history, display_history
+
+class TestMorseCodeTranslator(unittest.TestCase):
+
+# Test case 1: Valid English text to Morse code conversion
+    def test_english_to_morse(self):
+        english_text = "COVENTRY UNIVERSITY"
+        expected_morse_code = "-.-. --- ...- . -. - .-. -.--   ..- -. .. ...- . .-. ... .. - -.--"
+        self.assertEqual(text_to_morse(english_text), expected_morse_code)
