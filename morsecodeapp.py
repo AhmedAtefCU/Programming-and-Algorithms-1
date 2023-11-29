3006da0983132b104dd66deb66386af87f894a3b
Ahmed Atef <AA2201416@tkh.edu.eg>
2023-11-29 14:25:50 +0200

    Update morsecodeapp.py

--- morsecodeapp.py
+++ morsecodeapp.py
@@ -21,3 +21,22 @@ def display_menu():
     print("2. Enter a Text to Translate from Morse Code to English")
     print("3. Display Translation History")
     print("4. Exit")
+    
+# Function to convert text to Morse code
+def text_to_morse(text):
+    # Handling empty input error for encryption
+    if text.strip() == '':
+        raise ValueError("Cannot Encrypt Empty Space")
+    
+    morse_code = ""
+    for char in text.upper():
+        # Converts each character of the text to its Morse code representation
+        if char in morse_code_dict:
+            morse_code += morse_code_dict[char] + " "
+        elif char == ' ':
+            # For spaces, adds a space to maintain the distinction between words
+            morse_code += " "
+        else:
+            # If an invalid character is encountered, prompts the user to enter a valid letter
+            return "Please Enter a Valid Letter"
+    return morse_code.strip()
