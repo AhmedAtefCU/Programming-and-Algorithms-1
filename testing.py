9869f69666ec3362e278c8e36c3ab8bc665635a8
Ahmed Atef <AA2201416@tkh.edu.eg>
2023-11-29 16:14:03 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,3 +6,6 @@ from unittest.mock import patch
 
 # Library for capturing output
 from io import StringIO
+
+# Import the functions and dictionary from the Morse code translator script
+from morsecodeapp import text_to_morse, morse_to_text, morse_code_dict, history, display_history
