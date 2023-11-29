785086bb560a0e0145d26adfafed2d62e7d8b430
Ahmed Atef <AA2201416@tkh.edu.eg>
2023-11-29 14:24:31 +0200

    Update morsecodeapp.py

--- morsecodeapp.py
+++ morsecodeapp.py
@@ -13,3 +13,11 @@ morse_code_dict = {
 
 # List to store translation history
 history = []
+
+# Function to display the menu options
+def display_menu():
+    print("Menu:")
+    print("1. Enter a Text to Translate from English to Morse Code")
+    print("2. Enter a Text to Translate from Morse Code to English")
+    print("3. Display Translation History")
+    print("4. Exit")
